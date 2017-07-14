10027907764c8e2d5881be73ec03a5b8a70754e7
nickhartjenstein <hartjenstein@Hartjensteins.local>
2017-07-15 00:33:25 +0200

    *

--- src/js/search.js
+++ src/js/search.js
@@ -1,4 +1,6 @@
-window.location.href = "http://localhost:3000/?#search";
+
+// window.location.href = "http://localhost:3000/?#search";
+window.location.href = "http://hartjenstein.github.io/wiki_search_vanilla?#search";
 function getResults(){
   //  window.location.href = "http://localhost:3000/?#search";
     $.ajax({
